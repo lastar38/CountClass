9bfd0337d064b5a960cbc3c722f585492e2b21b6
lastar38 <lastarmain@gmail.com>
2021-03-04 21:28:43 +0900

    新規作成

---
+++ CountClass/CountClass/CountClass.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace CountClass
+{
+    class CountClass
+    {
+        private string FileName { get; set; }
+
+        public CountClass(string filename)
+        {
+            FileName = filename;
+        }
+
+        public int Count()
+        {
+            int count = 0;
+
+            if (File.Exists(FileName))
+            {
+                count = File.ReadLines(FileName, Encoding.UTF8)
+                            .Count(s => s.Contains("class"));
+
+                //var lines = File.ReadAllLines(FileName, Encoding.UTF8);
+                //foreach (var line in lines)
+                //{
+                //    if (line.Contains("class"))
+                //    {
+                //        count++;
+                //    }
+                //}
+
+                //using (var stream = new StreamReader(FileName, Encoding.UTF8))
+                //{
+                //    while (!stream.EndOfStream)
+                //    {
+                //        var line = stream.ReadLine();
+                //        if (line.Contains("class"))
+                //        {
+                //            count++;
+                //        }
+                //    }
+                //}
+            }
+           
+            return count;            
+        }
+    }
+}
---
+++ CountClass/CountClass/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var countclass = new CountClass(@"C:\Users\雄基\source\repos\Sample2\Sample2\Program.cs");
+            Console.WriteLine(countclass.Count());
+        }
+    }
+}
